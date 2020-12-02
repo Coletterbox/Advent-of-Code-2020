6704610c8f8c39996dd6d4864ec546fe73a85e84
Colette <colette.rs.shaw@gmail.com>
2020-12-02 17:29:34 +0000

    Remove comment

--- Day 2 Puzzle 1/src/main/java/placeholder/placeholder/App.java
+++ Day 2 Puzzle 1/src/main/java/placeholder/placeholder/App.java
@@ -2,10 +2,6 @@ package placeholder.placeholder;
 
 import java.io.FileNotFoundException;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws FileNotFoundException {
