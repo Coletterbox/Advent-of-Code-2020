f87a88df9c1c825d7cc59009576a7c4d8c4520ae
Colette <colette.rs.shaw@gmail.com>
2020-12-02 21:54:45 +0000

    Write ChooseNumbers function for C# version

---
+++ Day 1 Puzzle 1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int[] ChooseNumbers(int[] InputArray)
+        {
+            int[] ResultArray = { 0, 0 };
+
+            for (int i = 0; i < InputArray.Length; i++)
+            {
+                for (int j = 0; j < InputArray.Length; j++)
+                {
+                    if (i != j) // this is only necessary in the event that the input contains one instance of 1010
+                    {
+                        if (InputArray[i] + InputArray[j] == 2020)
+                        {
+                            ResultArray[0] = InputArray[i];
+                            ResultArray[1] = InputArray[j];
+                        }
+                    }
+                }
+            }
+
+            return ResultArray;
+        }
+    }
+}
