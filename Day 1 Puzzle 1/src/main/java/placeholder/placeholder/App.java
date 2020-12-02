5f7f717aa4dc9c36df5197e1dd4a9d34134ed425
Colette <colette.rs.shaw@gmail.com>
2020-12-02 02:42:52 +0000

    Write second test

--- Day 1 Puzzle 1/src/main/java/placeholder/placeholder/App.java
+++ Day 1 Puzzle 1/src/main/java/placeholder/placeholder/App.java
@@ -4,8 +4,7 @@ package placeholder.placeholder;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- Day 1 Puzzle 1/src/test/java/placeholder/placeholder/AppTest.java
+++ Day 1 Puzzle 1/src/test/java/placeholder/placeholder/AppTest.java
@@ -4,14 +4,19 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class AppTest 
-{
+public class AppTest {
     int[] testInput = {1721, 979, 366, 299, 675, 1456};
 
     @Test
-    public void chooseNumbersReturnsArray()
-    {
+    public void chooseNumbersReturnsSomething() {
         NumberChooser numberChooser = new NumberChooser();
-        assertTrue( NumberChooser.chooseNumbers(testInput) instanceof int[]  );
+        assertTrue(numberChooser.chooseNumbers(testInput) != null);
+    }
+
+    @Test
+    public void chosenNumbersEqual2020() {
+        NumberChooser numberChooser = new NumberChooser();
+        int[] resultArray = numberChooser.chooseNumbers(testInput);
+        assertTrue(resultArray[0] + resultArray[1] == 2020);
     }
 }
