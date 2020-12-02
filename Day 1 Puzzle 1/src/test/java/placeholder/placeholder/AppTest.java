dac73d1db5698e91e492a078b3baed1585bb4f95
Colette <colette.rs.shaw@gmail.com>
2020-12-02 02:58:36 +0000

    Write more tests

--- Day 1 Puzzle 1/src/test/java/placeholder/placeholder/AppTest.java
+++ Day 1 Puzzle 1/src/test/java/placeholder/placeholder/AppTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 public class AppTest {
     int[] testInput = {1721, 979, 366, 299, 675, 1456};
+    int[] testInput2 = {3, 2018, 2000, 300, 20};
 
     @Test
     public void chooseNumbersReturnsSomething() {
@@ -26,5 +27,19 @@ public class AppTest {
         NumberChooser numberChooser = new NumberChooser();
         int[] resultArray = numberChooser.chooseNumbers(testInput);
         assertEquals(514579, NumberMultiplier.multiplyNumbers(resultArray));
+    }
+
+    @Test
+    public void chosenNumbersEqual2020Again() {
+        NumberChooser numberChooser = new NumberChooser();
+        int[] resultArray = numberChooser.chooseNumbers(testInput2);
+        assertTrue(resultArray[0] + resultArray[1] == 2020);
+    }
+
+    @Test
+    public void checkProductOfNumbersAgain() {
+        NumberChooser numberChooser = new NumberChooser();
+        int[] resultArray = numberChooser.chooseNumbers(testInput2);
+        assertEquals(40000, NumberMultiplier.multiplyNumbers(resultArray));
     }
 }
