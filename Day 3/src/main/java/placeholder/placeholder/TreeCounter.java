e1ffde61c8d12286daa840d9d5a4b0085963fad8
Colette <colette.rs.shaw@gmail.com>
2020-12-03 12:52:04 +0000

    Start TreeCounter

--- Day 3/src/main/java/placeholder/placeholder/TreeCounter.java
+++ Day 3/src/main/java/placeholder/placeholder/TreeCounter.java
@@ -1,9 +1,18 @@
 package placeholder.placeholder;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class TreeCounter {
-    public int CountTrees(int x, int y) {
+    public int CountTrees(String filepath, int x, int y) throws IOException {
         // for this task, x will be 3 and y will be 1
         int treeCount = 0;
+
+        InputConverter inputConverter = new InputConverter();
+        ArrayList<String> inputAsArray = inputConverter.convertInputToArray(filepath);
+
+        // travel until bottom of the map; map tiles horizontally
+        // i.e. if you hit the end of a string, you appear at the beginning of the string
 
         return treeCount;
     }
