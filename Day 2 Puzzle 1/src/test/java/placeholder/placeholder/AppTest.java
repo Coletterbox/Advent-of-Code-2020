e3ee5075d20fd6aff4fcb85265cb6a9f37b3ac55
Colette <colette.rs.shaw@gmail.com>
2020-12-03 03:42:08 +0000

    Add second test

--- Day 2 Puzzle 1/src/test/java/placeholder/placeholder/AppTest.java
+++ Day 2 Puzzle 1/src/test/java/placeholder/placeholder/AppTest.java
@@ -10,8 +10,14 @@ import java.io.FileNotFoundException;
 public class AppTest 
 {
     @Test
-    public void shouldAnswerWithTrue() throws FileNotFoundException {
+    public void testUsingInput() throws FileNotFoundException {
         ValidPasswordCounter validPasswordCounter = new ValidPasswordCounter();
         assertEquals(622, validPasswordCounter.countValidPasswords("src/resources/input"));
+    }
+
+    @Test
+    public void testUsingTestInput() throws FileNotFoundException {
+        ValidPasswordCounter validPasswordCounter = new ValidPasswordCounter();
+        assertEquals(2, validPasswordCounter.countValidPasswords("src/resources/testInput"));
     }
 }
