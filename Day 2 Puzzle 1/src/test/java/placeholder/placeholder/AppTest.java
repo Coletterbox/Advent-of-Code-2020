09b24f6185c1fd4441d2bb6f3c62a8a644218ff7
Colette <colette.rs.shaw@gmail.com>
2020-12-03 03:36:28 +0000

    Add test for input

--- Day 2 Puzzle 1/src/test/java/placeholder/placeholder/AppTest.java
+++ Day 2 Puzzle 1/src/test/java/placeholder/placeholder/AppTest.java
@@ -1,14 +1,18 @@
 package placeholder.placeholder;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+
 public class AppTest 
 {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() throws FileNotFoundException {
+        ValidPasswordCounter validPasswordCounter = new ValidPasswordCounter();
+        validPasswordCounter.countValidPasswords();
+        assertEquals(622, validPasswordCounter.countValidPasswords());
     }
 }
