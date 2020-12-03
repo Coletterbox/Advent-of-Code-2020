419b10de20b90e0d1667610cd5a6f9644431dded
Colette <colette.rs.shaw@gmail.com>
2020-12-03 15:07:51 +0000

    Add display names to tests

--- Day 3/src/test/java/placeholder/placeholder/AppTest.java
+++ Day 3/src/test/java/placeholder/placeholder/AppTest.java
@@ -4,18 +4,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import java.io.IOException;
 
 public class AppTest {
 
     @Test
-    public void checkConvertInputToArray() throws IOException {
-        InputConverter inputConverter = new InputConverter();
-        inputConverter.convertInputToArray("src/resources/input");
-    }
-
-    @Test
+    @DisplayName("Test for small input that does not need to tile.")
     public void checkTreeCounterForInputThatDoesNotNeedToTile() throws IOException {
         TreeCounter treeCounter = new TreeCounter();
         int treeCount = treeCounter.CountTrees("src/resources/testInput",3, 1);
@@ -23,6 +19,7 @@ public class AppTest {
     }
 
     @Test
+    @DisplayName("Test for required puzzle input.")
     public void checkTreeCounter0() throws IOException {
         TreeCounter treeCounter = new TreeCounter();
         int treeCount = treeCounter.CountTrees("src/resources/input",3, 1);
