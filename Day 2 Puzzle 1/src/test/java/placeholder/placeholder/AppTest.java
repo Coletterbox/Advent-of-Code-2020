9719df7b53d24c3887352c9308419a6692b5ad38
Colette <colette.rs.shaw@gmail.com>
2020-12-03 03:48:20 +0000

    Add comment

--- Day 2 Puzzle 1/src/test/java/placeholder/placeholder/AppTest.java
+++ Day 2 Puzzle 1/src/test/java/placeholder/placeholder/AppTest.java
@@ -3,12 +3,21 @@ package placeholder.placeholder;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.io.FileNotFoundException;
 
 public class AppTest 
 {
+//    @BeforeEach
+//    public void setup() {
+//        ValidPasswordCounter validPasswordCounter = new ValidPasswordCounter();
+//    }
+
     @Test
     public void testUsingInput() throws FileNotFoundException {
         ValidPasswordCounter validPasswordCounter = new ValidPasswordCounter();
