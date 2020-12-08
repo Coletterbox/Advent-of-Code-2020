0121d7f8664c69eb2fcb7bec9b0205447e331c30
Colette <colette.rs.shaw@gmail.com>
2020-12-08 02:26:35 +0000

    Set up day 5

---
+++ Day 5/src/main/java/placeholder/placeholder/InputParser.java
@@ -0,0 +1,20 @@
+package placeholder.placeholder;
+
+public class InputParser {
+    public void convertInputToArray(String filepath) {
+
+    }
+    // remember that convertInputToArray will produce a string array, but the next functions deal with the individual strings
+    public int findRowNumber(String boardingPassSequence) {
+
+        return 0;
+    }
+    public int findColumnNumber(String boardingPassSequence) {
+
+        return 0;
+    }
+    public int findSeatID(int rowNumber, int columnNumber) {
+
+        return 0;
+    }
+}
