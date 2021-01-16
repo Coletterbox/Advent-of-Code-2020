59bf11987cf836502cd2da34455fd0063d749ea3
Colette <colette.rs.shaw@gmail.com>
2021-01-16 05:31:54 +0000

    Split followCommands

--- Day 8/src/main/java/placeholder/placeholder/CommandFollower.java
+++ Day 8/src/main/java/placeholder/placeholder/CommandFollower.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CommandFollower {
+
     void followCommands(String filename) throws IOException {
         InputParser inputParser = new InputParser();
         ArrayList<String> commandList = inputParser.convertInput(filename);
@@ -17,6 +18,23 @@ public class CommandFollower {
         for (int i = 0; i < commandList.size(); i++) {
             String[] operationAndArgument = commandList.get(i).split(" ");
             System.out.println(Arrays.toString(operationAndArgument));
+
+            followCommand(operationAndArgument);
         }
     }
+
+    void followCommand(String[] operationAndArgument) {
+        String operation = operationAndArgument[0];
+        String argument = operationAndArgument[1];
+        String sign = argument.substring(0, 1);
+        String value = argument.substring(1);
+
+        System.out.println(operation);
+        System.out.println(sign);
+        System.out.println(value);
+    }
+
+//    boolean[] test() throws IOException {
+//        return followCommands("exampleInput").commandsRun;
+//    }
 }
--- Day 8/src/test/java/placeholder/placeholder/AppTest.java
+++ Day 8/src/test/java/placeholder/placeholder/AppTest.java
@@ -2,11 +2,21 @@ package placeholder.placeholder;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class AppTest {
+    private static CommandFollower commandFollower;
+
+    @BeforeClass
+    public static void setup() throws IOException {
+        commandFollower = new CommandFollower();
+        commandFollower.followCommands("src/resources/exampleInput");
+    }
+
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue( true );
@@ -17,4 +27,11 @@ public class AppTest {
         CommandFollower commandFollower = new CommandFollower();
         commandFollower.followCommands("src/resources/exampleInput");
     }
+
+//    @Test
+//    public void testTesting() throws IOException {
+////        CommandFollower commandFollower = new CommandFollower();
+////        commandFollower.followCommands("src/resources/exampleInput");
+//        commandFollower.getValue("accumulator");
+//    }
 }
